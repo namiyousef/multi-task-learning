3ba0c319f4bb0386f8a1df32fa036fd359e11e03
ConnorWatts <67272820+ConnorWatts@users.noreply.github.com>
2021-12-23 22:35:00 +0000

    Add files via upload

---
+++ v1/data/datasets-oxpet/README.md
@@ -0,0 +1,6 @@
+# datasets
+
+Small data sets for demo, coursework, tutorials etc.
+
+N.B. individual data sets are in branches and can be accessed via:
+git clone -b mydataset --single-branch https://weisslab.cs.ucl.ac.uk/WEISSTeaching/datasets.git
---
+++ v1/models/utils.py
@@ -0,0 +1,26 @@
+import torch
+from models.resnet import resnet18
+from models.heads import ClassificationHead, SegmentationHead
+
+def get_body():
+
+    PRE_TRAINED = True
+    shared_net = resnet18(PRE_TRAINED)
+    shared_net_chan = 512
+    return shared_net ,shared_net_chan
+    
+
+def get_heads(config,tasks,encoder_chan):
+
+    return torch.nn.ModuleDict({task: get_head(config, encoder_chan, task) for task in tasks})
+
+def get_head(config, encoder_chan, task): 
+
+    if task == "Class":
+        return ClassificationHead(config['Tasks'][task],encoder_chan)
+
+    if task == "Seg":
+        return SegmentationHead(config['Tasks'][task],encoder_chan)
+
+    if task == "BB":
+        return ClassificationHead(config['Tasks'][task],encoder_chan)
