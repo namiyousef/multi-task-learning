5283fd9196fe23f864b4ca4f2cc2e9fb82c7d702
Yakov <yakovsushenok@protonmail.com>
2022-01-05 21:16:54 +0300

    adding data file

--- v1/data/data.py
+++ v1/data/data.py
@@ -1,12 +1,16 @@
+#from torch._C import double
+import torch
 from torch.utils.data import Dataset, DataLoader
+import torch.utils.data as data
 import h5py
 import os
 
 class OxfordPetDataset(Dataset):
 
     def __init__(self,config,split):
-
-        root = "./data"
+        
+        self.split = split
+        root = "./data/"
         root = root + split
         
         img_path = r'images.h5'
@@ -19,34 +23,43 @@ class OxfordPetDataset(Dataset):
         self.bbox_dir = os.path.join(root, bbox_path)
         self.bin_dir = os.path.join(root, bin_path)
 
-        # these return True if the strings of the respective task name is in the dictionary config, which is a dictionary of the tasks
-        self.seg_task= "SEGSEM" in config["Tasks"].keys()
+        self.seg_task= "Segmen" in config["Tasks"].keys()
         self.bb_task= "BB" in config["Tasks"].keys()
         self.bin_task= "Class" in config["Tasks"].keys()
 
 
     def __getitem__(self,index):
-        # Instead of returning a tensor we build a dictionary with the data corresponding to the tasks we have 
         sample = {}
-        # images are always loaded as well as the segmentation masks
+
         _img = self._load_data(index,self.image_dir)
-        sample['image'] = _img
-        _seg = self._load_data(index,self.seg_dir)
-        sample['seg'] = _seg
-        # Selectively adding the relevant tasks
+        sample['image'] = torch.from_numpy(_img).float()
+
+        if self.seg_task:
+            _seg = self._load_data(index,self.seg_dir)
+            sample['Segmen'] = torch.from_numpy(_seg).float()
+            
+
         if self.bb_task:
             _bb = self._load_data(index,self.bbox_dir)
-            sample['bb'] = _bb 
+            sample['BB'] = torch.from_numpy(_bb).float()
+             
+
         if self.bin_task:
             _bin = self._load_data(index,self.bin_dir)
-            sample['bin'] = _bin 
+            sample['Class'] = torch.from_numpy(_bin).float()
+             
 
         return sample  
 
-    # def __len__(self):
-    #     return len()
+    def __len__(self):
+        if self.split == "test":
+            return 738
+        elif self.split == "val":
+            return 738
+        elif self.split == "train":
+            return 32
 
-    def _load_data(index,dir):
+    def _load_data(self,index,dir):
         with  h5py.File(dir , 'r') as file:
             key = list(file.keys())[0]
             elems = file[key][ index]
