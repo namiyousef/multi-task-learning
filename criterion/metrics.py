eaabd5651d73dd7418ababf89d251d90fe6647f1
Yousef <namiyousef@hotmail.com>
2022-01-09 22:37:01 +0000

    fixes bug for accuracy

--- criterion/metrics.py
+++ criterion/metrics.py
@@ -1,2 +1,5 @@
+import torch
+
 def accuracy(y_true, y_pred):
-    return (y_true == y_pred).sum()/len(y_true)
+    y_class = torch.argmax(y_pred, dim=1)
+    return (y_true == y_class).sum()/len(y_true)
