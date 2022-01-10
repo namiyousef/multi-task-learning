557779f9809fc3795328194ad99ec9cfcbef63e1
Yousef <namiyousef@hotmail.com>
2022-01-10 00:45:19 +0000

    #9 adds HardMTLModel general class

--- models/model.py
+++ models/model.py
@@ -12,8 +12,43 @@ class Model(nn.Module):
         self.encoder, self.encoder_chan = get_body(filters)
         self.decoders = get_heads(config, self.tasks, self.encoder_chan,filters)
 
-
     def forward(self, x):
         output, skips = self.encoder(x)
-        return {task:self.decoders[task](output, skips) for task in self.tasks }
+        return {task:self.decoders[task](output, skips) for task in self.tasks}
 
+
+class HardMTLModel(nn.Module):
+    """Base MTL model. Builds MTL from a single encoder and can have multiple decoders
+    :param encoder:
+    :type encoder:
+    :param decoder:
+    :type decoder:
+    :param weights:
+    :type weights:
+    """
+    def __init__(self, encoder, decoders, weights=None):
+        super(HardMTLModel, self).__init__()
+        self.encoder = encoder
+        self.decoders = torch.nn.ModuleDict({task: decoder for task, decoder in decoders.items()})
+        self.weights = [weight for weight in weights] if weights is not None else [1] * len(decoders)
+
+    def forward(self, x):
+        output, skips = self.encoder(x) # TODO the encoder MAY not accept skips... need to make this robust
+        return {
+            task: decoder(output, skips) if decoder.has_skips else decoder(output) for task, decoder in self.decoders.items()
+        }
+
+def resnet34_class():
+    pass
+
+def resnet34_seg():
+    pass
+
+def resnet34_seg_class():
+    pass
+
+def resnet34_seg_class_bb():
+    pass
+
+def resnet34_seg_bb():
+    pass
