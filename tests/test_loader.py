36464183db8683a3a88bfa70ab00351847154ce9
Yousef <namiyousef@hotmail.com>
2022-01-11 23:50:42 +0000

    #14 adds complete testing to loader

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,10 +1,9 @@
-from data.data import OxfordPetDataset, OxpetDataset, OxpetDatasetIterable
+from data.data import OxfordPetDataset, OxpetDataset, RandomBatchSampler
 from torch.utils.data import DataLoader, IterableDataset, BatchSampler, SequentialSampler, RandomSampler, SubsetRandomSampler, Sampler
 import unittest
 import torch
 import time
 import h5py
-import math
 
 def _load_data(index, dir):
     with  h5py.File(dir, 'r') as file:
@@ -14,74 +13,110 @@ def _load_data(index, dir):
 
 class TestLoader(unittest.TestCase):
 
-    """def test_loaders_equal(self):
+    def test_datasets_equal(self):
+        """Using the same loader, tests that both datasets are equal
+        """
+        # prepare first dataset
         split = 'train'
-        loader1 = OxfordPetDataset(config={'Tasks':{
+        dset1 = OxfordPetDataset(config={'Tasks':{
             'Class':0, 'Segmen':0, 'BB':0
         }},split=split, mini_batch_size=0)
 
+        # prepare second dataset
         dir_path = '../datasets/data_new/train/'
-        loader2 = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
-        assert len(loader2) == len(loader1)
+        dset2 = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
+        assert len(dset2) == len(dset1)
+
         batch_size = 32
-        dloader1 = DataLoader(loader1, batch_size, shuffle=False)
-        dloader2 = DataLoader(loader2, batch_size, shuffle=False)
-        for (inputs1, targets1), batch2 in zip(dloader2, dloader1):
-            assert torch.equal(inputs1, batch2['image'])
-            break
+        dloader1 = DataLoader(dset1, batch_size, shuffle=False)  # set shuffle to false to ensure same loading
+        dloader2 = DataLoader(dset2, batch_size, shuffle=False)
+        for (inputs1, targets1), batch_dict in zip(dloader2, dloader1):
+            assert torch.equal(inputs1, batch_dict['image'])
 
+    def test_new_dataset_speed(self):
+        """tests that new dataset is faster
+        """
 
-    def test_old_loader_perf(self):
+        # prepare first dataset
         split = 'train'
-        loader = OxfordPetDataset(config={'Tasks': {
+        dset1 = OxfordPetDataset(config={'Tasks': {
             'Class': 0, 'Segmen': 0, 'BB': 0
         }}, split=split, mini_batch_size=0)
-        batch_size = 32
-        loader = DataLoader(loader, batch_size, shuffle=False)
-        s = time.time()
-        for batch in loader:
-            break
-        print(f'avg. time: {(time.time() - s)}')"""
 
-
-    """def test_new_loader_perf(self):
+        # prepare second dataset
         dir_path = '../datasets/data_new/train/'
-        loader = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
-        batch_size = 1
-        loader = DataLoader(loader, batch_size, shuffle=False)
-        s = time.time()
-        for batch in loader:
-            print(batch.shape)
-        print(f'avg. time: {(time.time() - s)}')"""
+        dset2 = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
+        assert len(dset2) == len(dset1)
 
+        batch_size = 32
+        limit = 10
+        dloader1 = DataLoader(dset1, batch_size, shuffle=False)  # set shuffle to false to ensure same loading
+        dloader2 = DataLoader(dset2, batch_size, shuffle=False)
+        s = time.time()
+        for i, (inputs1, targets1) in enumerate(dloader2, 1):
+            if i == limit:
+                break
+        dloader2_time = (time.time() - s) / limit
+
+        s = time.time()
+        for i, batch in enumerate(dloader1, 1):
+            if i == limit:
+                break
+        dloader1_time = (time.time() - s) / limit
+
+        print(f'Old dloader time: {dloader1_time}')
+        print(f'New dloader time: {dloader2_time}')
+
+        assert dloader2_time > dloader1_time
+
+    def test_batch_sampling(self):
+        dir_path = '../datasets/data_new/train/'
+        dataset = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
+        batchloader = DataLoader(dataset, batch_size=None, sampler=BatchSampler(SequentialSampler(dataset), batch_size=32, drop_last=False))
+        normalloader = DataLoader(dataset, batch_size=32, drop_last=False)
+        for (inputs1, targets1), (inputs2, targets2) in zip(batchloader, normalloader):
+            assert torch.equal(inputs1, inputs2)
+            for target1, target2 in zip(targets1.values(), targets2.values()):
+                assert torch.equal(target1, target2)
+
+    def test_random_batch_sampling_out_shuffle(self):
+        dir_path = '../datasets/data_new/train/'
+        dataset = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
+        torch.manual_seed(0)
+
+        batchloader = DataLoader(dataset, batch_size=None,
+                                 sampler=BatchSampler(RandomBatchSampler(dataset, batch_size=32, in_batch_shuffle=False), batch_size=32, drop_last=False))
+        ids = iter(torch.randperm(len(dataset)//32))
+        for i, (inputs, targets) in enumerate(batchloader):
+            if i < len(ids):
+                id = ids[i]
+            else:
+                id = 69
+            index = list(range(id*32, (id+1)*32))
+            inputs_file = _load_data(index, dir_path+'images.h5')
+            assert torch.equal(inputs_file, inputs)
+
+    def test_random_batch_sampling_in_shuffle_true(self):
+        dir_path = '../datasets/data_new/train/'
+        dataset = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
+        torch.manual_seed(0)
+
+        batchloader = DataLoader(dataset, batch_size=None,
+                                 sampler=BatchSampler(
+                                     RandomBatchSampler(dataset, batch_size=32, in_batch_shuffle=True), batch_size=32,
+                                     drop_last=False))
+        ids = iter(torch.randperm(len(dataset) // 32))
+        for i, (inputs, targets) in enumerate(batchloader):
+            if i < len(ids):
+                id = ids[i]
+            else:
+                id = 69
+            index = list(range(id * 32, (id + 1) * 32))
+            inputs_file = _load_data(index, dir_path + 'images.h5')[torch.randperm(len(index))]
+            assert torch.equal(inputs_file, inputs)
 
 
     def test_iterable_data_loader(self):
-        class RandomBatchSampler(Sampler):
-            def __init__(self, dataset, batch_size):
-                self.batch_size = batch_size
-                self.dataset_length = len(dataset)
-                self.n_batches = self.dataset_length / self.batch_size
-
-                self.batch_ids = torch.randperm(int(self.n_batches))
-
-            def __len__(self):
-                return self.batch_size
-
-            def __iter__(self):
-                for id in self.batch_ids:
-                    for index in range(id*self.batch_size, (id+1)*self.batch_size):
-                        yield index
-                if int(self.n_batches) < self.n_batches:
-                    for index in range(int(self.n_batches)*self.batch_size, self.dataset_length):
-                        yield index
-
-        dir_path = '../datasets/data_new/train/'
-        loader = OxpetDataset(dir_path, ['class', 'seg', 'bb'])
-        a = DataLoader(loader, batch_size=None, sampler=RandomBatchSampler(loader, batch_size=32))
-        a = DataLoader(loader, batch_size=None, sampler=BatchSampler(SequentialSampler(loader), batch_size=32, drop_last=False))
-        a = DataLoader(loader, batch_size=None, sampler=BatchSampler(RandomBatchSampler(loader, batch_size=32), batch_size=32, drop_last=False))
-
         split = 'train'
         data_old = OxfordPetDataset(config={'Tasks': {
             'Class': 0, 'Segmen': 0, 'BB': 0
