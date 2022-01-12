4069975c524d91cdeeb30bf4a5fcd68f43902211
Yousef <namiyousef@hotmail.com>
2022-01-12 19:54:19 +0000

    #20 adds some default configs, #9 generalises HardMTLModel for inputs with skips

--- models/model.py
+++ models/model.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
-from models.utils import get_body, get_heads, get_head
-import torch.nn.functional as F
+from models.heads import ClassificationHead, SegmentationHead, BBHead
+from models.utils import get_body, get_heads
+from models.resnet import resnet34
 
 class Model(nn.Module):
     
@@ -13,6 +14,7 @@ class Model(nn.Module):
         self.decoders = get_heads(config, self.tasks, self.encoder_chan,filters)
 
     def forward(self, x):
+        print(type(self.encoder(x)))
         output, skips = self.encoder(x)
         return {task:self.decoders[task](output, skips) for task in self.tasks}
 
@@ -32,14 +34,21 @@ class HardMTLModel(nn.Module):
         self.decoders = torch.nn.ModuleDict({task: decoder for task, decoder in decoders.items()})
 
     def forward(self, x):
-        output, skips = self.encoder(x) # TODO the encoder MAY not accept skips... need to make this robust
+        outputs = self.encoder(x) # TODO the encoder MAY not accept skips... need to make this robust
+        # TODO need to add good documentation explaining how to use this
         return {
-            task: decoder(output, skips) if decoder.has_skips else decoder(output) for task, decoder in self.decoders.items()
+            task: decoder(*outputs) for task, decoder in self.decoders.items()
         }
 
 
-def resnet34_class():
-    pass
+def resnet34_class(pretrained=False):
+    encoder = resnet34(pretrained)
+    filters = [64, 128, 256, 512]
+    decoder_out_channels = filters[-1]
+    decoders = {
+        'class': ClassificationHead(in_channels=decoder_out_channels, num_classes=2),
+    }
+    return HardMTLModel(encoder, decoders)
 
 def resnet34_seg():
     pass
@@ -48,7 +57,37 @@ def resnet34_seg_class():
     pass
 
 def resnet34_seg_class_bb():
+
     pass
 
 def resnet34_seg_bb():
     pass
+
+def resnet34_seg_class():
+    encoder = resnet34(False)
+    filters = [64, 128, 256, 512]
+    decoder_out_channels = filters[-1]
+    decoders = {
+        'class': ClassificationHead(in_channels=decoder_out_channels, num_classes=2),
+        'seg': SegmentationHead(filters=filters),
+    }
+    return HardMTLModel(encoder, decoders)
+
+def resnet34_seg_bb():
+    encoder = resnet34(False)
+    filters = [64, 128, 256, 512]
+    decoder_out_channels = filters[-1]
+    decoders = {
+        'class':ClassificationHead(in_channels=decoder_out_channels, num_classes=2),
+        'bb': BBHead(in_channels=decoder_out_channels, num_classes=4)
+    }
+    return HardMTLModel(encoder, decoders)
+
+def resnet34_seg():
+    encoder = resnet34(False)
+    filters = [64, 128, 256, 512]
+    decoder_out_channels = filters[-1]
+    decoders = {
+        'seg': SegmentationHead(filters=filters),
+    }
+    return HardMTLModel(encoder, decoders)
