67fb1dbd7f550c90d8a1cf1bcf39e9034a80fed7
Yousef <namiyousef@hotmail.com>
2022-01-12 19:53:35 +0000

    #16 adds RandomCombinedLoss class, #9 fixes bugs with training loop, updates device management to allow multi-task inputs

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from models import model
 from criterion.criterion import Criterion
 from criterion.metric_functions import accuracy
 from criterion import metric_functions
-from data.data import get_dataloader, get_dataset
+from data.data import get_dataloader, get_dataset, RandomBatchSampler
 from train import model_train
 import time
+from data.data import OxpetDataset
+from torch.utils.data import DataLoader, BatchSampler
 
 configuration = {
         'save_params':'s',
@@ -55,23 +57,45 @@ class SimpleCombinedLoss(torch.nn.Module):
 class CombinedLoss(torch.nn.Module):
     # TODO solve loss problem by subclassing from 'combined loss' class
     def __init__(self):
+        super(CombinedLoss, self).__init__()
         self.is_combined_loss = True
         self.losses = None
     def update_history(self, loss_history):
         pass
 
 
-class RandomCombinedLoss(torch.nn.Module):
-    # TODO think about how prior runs here, mauybe needs pre-init
-    def __init__(self, loss_dict, prior):
+class RandomCombinedLoss(CombinedLoss):
+    """Adds random weights to the loss before summation
+
+    :param loss_dict: dictionary with keys and values, {task_name: callable_loss}
+    :type loss_dict: dict
+    :param prior: partially initialised prob function, callable without any extra params.
+    :type prior: functools.partial
+    :param frequency: number of mini-batches before update. This should be math.ceil(data_length / batch_size)
+    :type frequency: int
+    """
+    def __init__(self, loss_dict, prior, frequency=1):
+        super(RandomCombinedLoss, self).__init__()
         self.loss_dict = loss_dict
         self.prior = prior
+        self.frequency = frequency
+        self.mini_batch_counter = 0
+        self.weights = None
+
     def forward(self, outputs, targets):
+        if self.mini_batch_counter % self.frequency == 0:
+            self._update_weights()
+        self.mini_batch_counter += 1
+        print(f'Epoch {self.mini_batch_counter // self.frequency}, minibatch {self.mini_batch_counter}: weights: {self.weights}')
         losses = {
-            task: weight * loss(outputs, targets) for (task, loss), weight in zip(self.loss_dict.items(), self.prior)
+            task: weight * loss(
+                outputs[task], targets[task]
+            ) for (task, loss), weight in zip(self.loss_dict.items(), self.weights)
         }
         return sum(losses.values())
 
+    def _update_weights(self):
+        self.weights = self.prior()
 
 class RunTorchModel:
     """
@@ -82,12 +106,12 @@ class RunTorchModel:
         self.optimizer = optimizer
         self.loss = loss # TODO need to think about how weights would work with this, in conjunction with history, e.g. getting each loss individually and the weights applied
         self.device = _get_device()
-        self.loss_history = {self.loss.__name__: []} # TODO how to update loss_history for combined and single losses
+        self.loss_history = {self.loss.__class__.__name__: []} # TODO how to update loss_history for combined and single losses
         self.metrics = metrics
         if hasattr(self.loss, 'is_combined_loss'):
-            self.metrics_history = {metric.__name__: [] for task_metrics in metrics for metric in task_metrics}
+            self.metrics_history = {metric.__class__.__name__: [] for task_metrics in metrics for metric in task_metrics}
         else:
-            self.metrics_history = {metric.__name__: [] for metric in metrics} # TODO also this is wrong, need to deal with multitask or single task case, maybe base on loss subclass?
+            self.metrics_history = {metric.__class__.__name__: [] for metric in metrics} # TODO also this is wrong, need to deal with multitask or single task case, maybe base on loss subclass?
 
     def train(self, trainloader, epochs=1, valloader=None, verbose=0, track_history=False):
 
@@ -101,8 +125,8 @@ class RunTorchModel:
             start_load = time.time()
             for i, (inputs, targets) in enumerate(trainloader):
                 start_train = time.time()
-                inputs = inputs.to(self.device)
-                targets = targets.to(self.device)
+                inputs = self._move(inputs)
+                targets = self._move(targets)
 
                 self.optimizer.zero_grad()
                 outputs = self.model(inputs)
@@ -110,17 +134,17 @@ class RunTorchModel:
                 # make this into a function, because we will need to use it in the testing loop as well, for both val and test
                 # make sure the only inputs you need are outputs, inputs. Make default storing of loss_history
                 if track_history:
-                    self.loss_history[self.loss.__name__].append(loss.item())
+                    self.loss_history[self.loss.__class__.__name__].append(loss.item())
                     if hasattr(self.loss, 'is_combined_loss'):
                         self.loss.update_history(self.loss_history)
                         for task in self.metrics:
                             metrics = self.metrics[task]
                             for metric in metrics:
-                                self.metrics_history[metric.__name__].append(metric(outputs, targets))
+                                self.metrics_history[metric.__class__.__name__].append(metric(outputs, targets))
                     else:
                         for metric in self.metrics:
-                            self.metrics_history[metric.__name__].append(metric(outputs, targets))
-
+                            self.metrics_history[metric.__class__.__name__].append(metric(outputs, targets))
+                            # TODO the indices print from dataset, need to remove that
                 loss.backward()
                 self.optimizer.step()
                 # metrics here
@@ -129,7 +153,7 @@ class RunTorchModel:
                 end_train = time.time()
                 if verbose == 2:
                     print(f'Batch {i+1} complete. Time taken: load({start_train - start_load}), '
-                          f'train({end_train - start_train}), total({end_train - start_load}). Loss: {}')
+                          f'train({end_train - start_train}), total({end_train - start_load}). Loss: {"ignore"}')
                 start_load = time.time()
             # TODO add valloader
 
@@ -140,7 +164,7 @@ class RunTorchModel:
 
             end_epoch = time.time()
             print_message = f'Epoch {epoch+1}/{epochs} complete. Time taken: {end_epoch - start_epoch:.3g}. ' \
-                            f'Loss: {}'
+                            f'Loss: {"ignore"}'
 
             if verbose:
                 print(f'{"-"*len(print_message)}')
@@ -158,6 +182,18 @@ class RunTorchModel:
 
     def history(self):
         pass
+
+    def _move(self, data):
+        if torch.is_tensor(data):
+            return data.to(self.device)
+        elif isinstance(data, dict):
+            return {task: tensor.to(self.device) for task, tensor in data.items()}
+        elif isinstance(data, list):
+            raise NotImplementedError('Currenlty no support for tensors stored in lists.')
+        else:
+            raise TypeError('Invalid data type.')
+
+
 
 
 
@@ -236,30 +272,30 @@ def main(config, epochs=1, batch_size=32,
     print(f'Test Accuracy: {test_accuracy/(i+1)}')
 
 if __name__ == '__main__':
-    import re
-    def _convert_class_to_func(cls_name):
-        cls_comps = re.findall('[A-Z][^A-Z]*', cls_name)
-        func_name = '_'.join([comp.lower() for comp in cls_comps])
-        return func_name
 
-    def _convert_func_to_class():
-        pass
-
-    def build_mtl_from_config(config):
-        encoder_name, encoder_params = configuration['encoder'].values()
-        # TODO to generalise this will have to change, for now we are just working with strings
-        encoder = getattr(bodys, encoder_name)(encoder_params)  # TODO maybe kwargs this for robustness
-        decoders = configuration['decoders']
-        decoder_dict = {
-            task: getattr(heads, decoder_info['name']) for task, decoder_info in decoders.items()
-        }
-        # TODO honestly, I don't like this at all. I want default models
-        # TODO add this
-        model = HardMTLModel(encoder, decoder_dict)
-        return model(encoder, decoders), loss
-
-    run = 'OLD'
+    run = 'NEW'
     if run == 'NEW':
+        from models.model import resnet34_class
+        from functools import partial
+
+        # model configuration
+        model = resnet34_class(False)
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-04)
+
+        loss = RandomCombinedLoss(loss_dict={'class': torch.nn.CrossEntropyLoss()}, prior=partial(torch.rand, size=(1,)), frequency=5)
+        # data
+        root_path = 'datasets/data_new/{}'
+        train_data = OxpetDataset(root_path.format('train'), tasks=['class'])
+        train_bs = 512
+        train_loader = DataLoader(
+            train_data, batch_size=None,
+            sampler=BatchSampler(RandomBatchSampler(train_data, train_bs), batch_size=train_bs, drop_last=False)
+        )
+
+        run_instance = RunTorchModel(model=model, optimizer=optimizer, loss=loss)
+        run_instance.train(train_loader, epochs=2, verbose=1)
+
+
         configuration = {
             'save_params': 's',
             'encoder': {
@@ -326,3 +362,27 @@ def _create_config_from_str(model_name):
     decoder_name = model_components
     task_names = model_components[1:]
     pass
+
+
+
+    import re
+    def _convert_class_to_func(cls_name):
+        cls_comps = re.findall('[A-Z][^A-Z]*', cls_name)
+        func_name = '_'.join([comp.lower() for comp in cls_comps])
+        return func_name
+
+    def _convert_func_to_class():
+        pass
+
+    def build_mtl_from_config(config):
+        encoder_name, encoder_params = configuration['encoder'].values()
+        # TODO to generalise this will have to change, for now we are just working with strings
+        encoder = getattr(bodys, encoder_name)(encoder_params)  # TODO maybe kwargs this for robustness
+        decoders = configuration['decoders']
+        decoder_dict = {
+            task: getattr(heads, decoder_info['name']) for task, decoder_info in decoders.items()
+        }
+        # TODO honestly, I don't like this at all. I want default models
+        # TODO add this
+        model = HardMTLModel(encoder, decoder_dict)
+        return model(encoder, decoders), loss
