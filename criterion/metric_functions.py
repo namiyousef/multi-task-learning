fb69f3fb2cdfb5baf563bfb9f932d1fd1e805308
Yousef <namiyousef@hotmail.com>
2022-01-15 17:35:52 +0000

    #23 adds segmentation metrics

--- criterion/metric_functions.py
+++ criterion/metric_functions.py
@@ -9,14 +9,91 @@ import torch
 # the metric that you are calculating. A simple example is shown below
 # make sure to think of edge cases as well.
 
-class Accuracy(torch.nn.Module):
-
+class MultiAccuracy(torch.nn.Module):
+    """Calculates accuracy for multiclass inputs (batchsize, feature length) by determining the most likely class
+    using argmax -> (batchsize,) and then comparing with targets which are also (batchsize,)
+    """
     def __init__(self):
-        super(Accuracy, self).__init__()
+        super(MultiAccuracy, self).__init__()
 
     def forward(self, outputs, targets):
         outputs = torch.argmax(outputs, dim=1)
-        # remember, the edge case here is that this would not work if you have a 2D matrix!
+        return torch.sum(outputs == targets) / len(targets)
+
+class PixelAccuracy(torch.nn.Module):
+    def __init__(self):
+        super(PixelAccuracy, self).__init__()
+
+    def forward(self, outputs, targets):
+        outputs = torch.where(outputs >= 0.5, 1.0, 0.0)
+        outputs = outputs.flatten()
+        targets = targets.flatten()
         return torch.sum(outputs == targets) / len(targets)
 
 
+class Jaccard(torch.nn.Module):
+
+    def __init__(self):
+        super(Jaccard, self).__init__()
+
+    def forward(self, outputs, targets):
+        outputs = torch.where(outputs >= 0.5, 1.0, 0.0)
+        outputs = outputs.flatten()
+        targets = targets.flatten()
+        intersection = torch.sum(outputs == targets)
+        union = len(targets) + len(outputs) - intersection
+        return intersection / union
+
+class Precision(torch.nn.Module):
+
+    def __init__(self):
+        super(Precision, self).__init__()
+
+    def forward(self, outputs, targets):
+        outputs = torch.where(outputs >= 0.5, 1.0, 0.0)
+        outputs = outputs.flatten()
+        targets = targets.flatten()
+
+        # filter only positive predictions
+        ids_pos = targets == 1.0
+
+        pos_targets = targets[ids_pos]
+        pos_outputs = outputs[ids_pos]
+        neg_targets = targets[~ids_pos]
+        neg_outputs = outputs[~ids_pos]
+        tp = torch.sum(pos_outputs == pos_targets)
+        fp = torch.sum(neg_targets != neg_outputs)
+        return tp/(tp+fp)
+
+class Recall(torch.nn.Module):
+    def __init__(self):
+        super(Recall, self).__init__()
+
+    def forward(self, outputs, targets):
+        outputs = torch.where(outputs >= 0.5, 1.0, 0.0)
+        outputs = outputs.flatten()
+        targets = targets.flatten()
+
+        # filter only positive predictions
+        ids_pos = targets == 1.0
+
+        pos_targets = targets[ids_pos]
+        pos_outputs = outputs[ids_pos]
+
+        tp = torch.sum(pos_outputs == pos_targets)
+        fn = torch.sum(pos_targets != pos_outputs)
+
+        return tp / (tp + fn)
+
+class F1Score(torch.nn.Module):
+    def __init__(self):
+        super(F1Score, self).__init__()
+        self.precision = Precision()
+        self.recall = Recall()
+
+    def forward(self, outputs, targets):
+
+        recall = self.recall(outputs, targets)
+        precision = self.precision(outputs, targets)
+
+        return 2 * (precision * recall) / (precision + recall)
