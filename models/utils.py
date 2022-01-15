4d9bef0eb8ac71c2d512a15f76024a7d4a3eeb08
Yousef <namiyousef@hotmail.com>
2022-01-15 17:35:08 +0000

    #9 removes legacy code, adds get_prebuilt model method

--- models/utils.py
+++ models/utils.py
@@ -1,51 +1,43 @@
-import torch
-from models.resnet import resnet34
-from models.heads import ClassificationHead, SegmentationHead, BBHead
-import torch.nn as nn
+from utils import _split_equation
+from criterion.loss_functions import RandomCombinedLoss, SimpleCombinedLoss, DynamicCombinedLoss
+from models import bodys
 
+def get_prebuilt_model(encoder, decoders, losses, weights=None, apply_weights_during_test=False):
 
+    decoders = _split_equation(decoders)
+    losses = _split_equation(losses)
+    scaling_factors = {task: float(loss.split('*')[0]) if '*' in loss else 1.0 for task, loss in
+                       zip(decoders, losses)}
+    losses = {task: loss.split('*')[-1] for task, loss in zip(decoders, losses)}
 
-class ConvLayer(nn.Module):
-    def __init__(self, input_dim, output_dim, stride, padding):
-        self.conv_block = nn.Sequential(
-            nn.BatchNorm2d(input_dim),
-            nn.ReLU(),
-            nn.Conv2d(
-                input_dim, output_dim, kernel_size=3, stride=stride, padding=padding
-            ),
-            nn.BatchNorm2d(output_dim),
-            nn.ReLU(),
-            nn.Conv2d(output_dim, output_dim, kernel_size=3, padding=1),
-        )
-        self.conv_skip = nn.Sequential(
-            nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=stride, padding=1),
-            nn.BatchNorm2d(output_dim),
-        )
+    if weights is None:
+        loss = SimpleCombinedLoss(losses, weights, sf=scaling_factors, eval_test=apply_weights_during_test)
+    if isinstance(weights, list):
+        if len(decoders) != len(weights):
+            raise ValueError(
+                'The number of tasks is different to the number of weights. Please make sure that decoders and weights have the same size.')
+        elif sum(weights) != 1:
+            raise ValueError('The sum of weights is greater than 1. Please make sure they sum up to 1.')
 
-    def forward(self, x):
+        loss = SimpleCombinedLoss(losses, weights, sf=scaling_factors, eval_test=apply_weights_during_test)
 
-        return self.conv_block(x) + self.conv_skip(x)
+    elif isinstance(weights, str):
+        weights = weights.split('::')
+        prior = weights[0]
+        if len(weights) < 2:
+            raise ValueError(
+                'You must include a parameter. For random loss weighting, this must be frequency. For dynamic loss weighting, this must be frequency and temperature')
 
+        try:
+            frequency = int(weights[1])
+        except:
+            TypeError('Frequency must be an integer')
+        if prior == 'dynamic':
+            temperature = float(weights[-1])
+            loss = DynamicCombinedLoss(losses, frequency=frequency, temperature=temperature)
+        else:
+            loss = RandomCombinedLoss(losses, frequency=frequency, prior=prior, sf=scaling_factors, eval_test=apply_weights_during_test)
 
-
-
-def get_body(filters):
-    # TODO so we're using entire resnet?? Not just the decoder?
-    shared_net = resnet34(False)
-    shared_net_chan = filters[3]
-    return shared_net ,shared_net_chan
-    
-def get_heads(config,tasks,encoder_chan,filters):
-
-    return torch.nn.ModuleDict({task: get_head(config, encoder_chan, task,filters) for task in tasks})
-
-def get_head(config, encoder_chan, task,filters): 
-
-    if task == "Class":
-        return ClassificationHead(encoder_chan,config['Tasks'][task])
-
-    if task == "Segmen":
-        return SegmentationHead(filters)
-
-    if task == "BB":
-        return BBHead(encoder_chan,config['Tasks'][task])
+    decoders = sorted(decoders)
+    model = getattr(bodys, f'{encoder}_{"_".join(decoders)}')()
+    return model, loss
